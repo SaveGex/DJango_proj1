c1dedd0edb5e3c12571b8934efe1dce35ee12705
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-07-17 21:41:02 +0300

    project settings

--- core/settings.py
+++ core/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "DJANGO1",
+        "USER": "common_user",
+        "PASSWORD": "2558",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
