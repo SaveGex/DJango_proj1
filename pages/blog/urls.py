7a03e2a15c79ecbbe4a1c552f4de7195f360771b
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-07-20 19:54:52 +0300

    add possibility append posts

--- pages/blog/urls.py
+++ pages/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_index, name='index'),
-    path('post/<int:post_id>/', views.detail, name='detail')
+    path('post/<int:post_id>/', views.detail, name='detail'),
+    path('create/', views.create_view, name= 'create')
 ]
--- pages/blog/views.py
+++ pages/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post 
+from .forms import PostForm
 # Create your views here.
 def blog_index(request):
 
     all_posts = Post.objects.all()
     
     context = {
-        "all_posts": all_posts
+        "all_posts": all_posts,
+        "create_form": PostForm()
     }
     return render(request, "blog/index.html", context) # kpei4ny genius of this program 
 
@@ -19,3 +21,9 @@ def detail(request, post_id):
         'post': post,
     }
     return render(request, 'blog/detail.html', context)
+def create_view(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:detail', post_id = post.id)
